b6c19f8e15f89570b84654d8221511178f913a04
MarquinFA <marcoantoniofelixdealmeida@gmail.com>
2025-06-27 10:49:50 -0300

    teste

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,33 @@
 import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
+
+import { connectToDatabase } from "./config/db.js";
+import { Book } from "./model/book.model.js";
 
 const app = express();
 
+app.use(express.json());
+
+app.post("/api/v1/books", async (req, res) => {
+  const { title, subtitle, author, genre, cover } = req.body;
+
+  try {
+    const book = new Book({ title, subtitle, author, genre, cover });
+    await book.save();
+    res.status(201).json({ success: true, data: book });
+  } catch (error) {
+    console.error("Error saving book: ", error);
+    res.status(500).json({ success: false, error: "Error saving book" });
+  }
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
+  connectToDatabase();
 });
+
+//user marco
+//senha: CQRV1GfTzlhfPvJP
+
+// mongodb+srv://marco:<db_password>@cluster0.j6pizsu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
