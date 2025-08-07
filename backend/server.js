fd5272f7e30e53faabd43d66c1a96bd884e66394
MarquinFA <marcoantoniofelixdealmeida@gmail.com>
2025-08-07 10:56:32 -0300

    passo 35

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,92 @@ app.post("/api/v1/books", async (req, res) => {
   }
 });
 
+app.get("/api/v1/books", async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.status(200).json({ success: true, data: books });
+  } catch (error) {
+    console.error("Error fetching books: ", error);
+    res.status(500).json({ sucess: false, error: "Error fetching books" });
+  }
+});
+
+app.get("/api/v1/books/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ sucess: false, error: "Invalid ID" });
+  }
+
+  try {
+    const book = await Book.findById(id);
+
+    if (!book) {
+      return res.status(404).json({ success: false, error: "Book not found" });
+    }
+
+    res.status(200).json({ sucess: true, data: book });
+  } catch (error) {
+    console.error("Error fetching book: ", error);
+    res.status(500).json({ success: false, error: "Error fetching book" });
+  }
+});
+
+app.put("/api/v1/books/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, subtitle, author, genre, cover } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ sucess: false, error: "Invalid ID" });
+  }
+
+  try {
+    const book = await Book.findById(id);
+
+    if (!book) {
+      return res.status(404).json({ success: false, error: "Book not found" });
+    }
+
+    const updatedBook = await Book.findByIdAndUpdate(
+      id,
+      { title, subtitle, author, genre, cover },
+      { new: true }
+    );
+
+    res.status(200).json({
+      success: true,
+      data: updatedBook,
+    });
+  } catch (error) {
+    console.error("Error updating book: ", error);
+    res.status(500).json({ success: false, error: "Error updating book" });
+  }
+});
+
+app.delete("/api/v1/books/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ sucess: false, error: "Invalid ID" });
+  }
+
+  try {
+    const book = await Book.findById(id);
+
+    if (!book) {
+      return res.status(404).json({ success: false, error: "Book not found" });
+    }
+
+    await Book.findByIdAndDelete(id);
+
+    res.status(200).json({ success: true, data: {} });
+  } catch (error) {
+    console.error("Error deleting book: ", error);
+    res.status(500).json({ success: false, error: "Error deleting book" });
+  }
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
   connectToDatabase();
 });
-//user: marcofelixalmeida
-//senha: xlLqTFhYFTw1eeuK
-//string: mongodb+srv://marcofelixalmeida:xlLqTFhYFTw1eeuK@cluster0.6keaper.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
